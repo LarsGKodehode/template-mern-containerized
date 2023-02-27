2f6e7210ad05acd9e1778d6bb8e8c62c166c07c1
LarsG <lars.ga.solheim@gmail.com>
2023-02-27 02:20:21 +0100

    api: update port

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,10 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
+const DB_URI = process.env.MONGODB_URI || null;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('Express + TypeScript Server. DB_URI' + DB_URI);
 });
 
 app.listen(port, () => {
