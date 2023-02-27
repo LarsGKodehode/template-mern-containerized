1d46740771ab4054fc69ba1d0df1758bbc838b13
LarsG <lars.ga.solheim@gmail.com>
2023-02-27 02:45:26 +0100

    api: connect db

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,8 +7,12 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 const DB_URI = process.env.MONGODB_URI || null;
 
+if (typeof DB_URI === 'string') {
+  fetch('http://' + DB_URI).then(() => console.log('DB connection is live'))
+}
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server. DB_URI' + DB_URI);
+  res.send('Express + TypeScript Server.');
 });
 
 app.listen(port, () => {
